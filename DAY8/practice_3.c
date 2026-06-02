898acbee80979d3fa57a00d9aaabbf7719f1e0d7
nandiniecilson <varshneynandinie@gmail.com>
2026-06-02 21:47:52 +0530

    PRAC_3

---
+++ DAY8/practice_3.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+    char n;
+    int i,j;
+    printf("Enter rows:");
+    scanf("%c", &n);
+
+    for(i='A'; i<=n; i++){
+        for(j='A'; j<=i; j++){
+            printf("%c", j);
+        }
+        printf("\n");
+    }
+    return 0;
+}
