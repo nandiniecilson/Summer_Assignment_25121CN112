e58c61ef751907cc395aebb554c84d0f7c932fff
nandiniecilson <varshneynandinie@gmail.com>
2026-06-24 11:17:55 +0530

    Character pyramid

---
+++ DAY10/practice_4.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int rows = 5;
+
+    for (int i = 1; i <= rows; i++) {
+        for (int j = i; j < rows; j++) {
+            printf(" ");
+        }
+
+        for (int j = 1; j <= i; j++) {
+            printf("%c", (char)(j + 64));
+        }
+        for (int j = i - 1; j >= 1; j--) {
+            printf("%c", (char)(j + 64));
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
